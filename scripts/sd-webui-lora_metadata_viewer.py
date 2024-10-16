58710191b6eae9940e3c7425f2a255d426c66bad
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-16 16:59:57 +0200

    Update sd-webui-lora_metadata_viewer.py

--- scripts/sd-webui-lora_metadata_viewer.py
+++ scripts/sd-webui-lora_metadata_viewer.py
@@ -76,14 +76,14 @@ def on_ui_tabs():
 script_callbacks.on_ui_tabs(on_ui_tabs)
 
 # Function get_lora_path().
-def get_lora_path(lora_file: str) -> str:
+def get_lora_path(lora_file: str):
     '''Get the path to the LoRA file.'''
     if not os.path.isfile(os.path.join(LORA_PATH, lora_file)):
         return None
     return os.path.join(LORA_PATH, lora_file)
 
 # Function read_lora_metadata().
-def read_lora_metadata(input_file: str) -> json:
+def read_lora_metadata(input_file: str):
     '''Read the LoRA metadata.'''
     if selected_model := get_lora_path(lora_dict.get(input_file)):
         if metadata := models.read_metadata_from_safetensors(selected_model):
