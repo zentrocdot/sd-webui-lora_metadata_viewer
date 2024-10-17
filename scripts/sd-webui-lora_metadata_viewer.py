b2eae679a03b16ff6ccb124d978e397d034fc406
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-17 17:23:10 +0200

    Update sd-webui-lora_metadata_viewer.py

--- scripts/sd-webui-lora_metadata_viewer.py
+++ scripts/sd-webui-lora_metadata_viewer.py
@@ -84,7 +84,7 @@ def on_ui_tabs():
                 new_list = get_lora_list()
                 input_file.update(choices=new_list, value=None)
                 return [out_state]
-            # sort_fw_bw.change(change_sort_fw_bw, inputs=[sort_fw_bw], outputs=[])
+            sort_fw_bw.change(change_sort_fw_bw, inputs=[sort_fw_bw], outputs=[])
             #def choices_change():
             #    new_list = get_lora_list()
             #    print(_SortDir)
